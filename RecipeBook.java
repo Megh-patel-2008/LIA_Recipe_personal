3e130a447ac51dbbf99f79a85b567e810a942d0e
Megh-patel-2008 <M3ghpatel2008@gmail.com>
2025-12-12 23:43:12 -0500

    Baking Frying RecipeBook

--- RecipeBook.java
+++ RecipeBook.java
@@ -7,8 +7,56 @@ import java.util.ArrayList;
  */
 public class RecipeBook
 {
+    private ArrayList<Recipe>recipes;
     public RecipeBook()
     {
-        
+        recipes = new ArrayList<>();
+    }
+    public void addRecipe(Recipe r)
+    {
+        recipes.add(r);
+    }
+    public Recipe findByTitle(String title)
+    {
+        for(Recipe r : recipes)
+        {
+            if(r.getTitle().equalsIgnoreCase(title.trim()))
+            {
+                return r;
+            }
+        }
+        return null;
+    }
+    public ArrayList<Recipe> getAllRecipes()
+    {
+        return recipes;
+    }
+    public ArrayList<Baking> getBakingByEgg(boolean containsEgg)
+    {
+        ArrayList<Baking> out = new ArrayList<>();
+        for(Recipe r : recipes)
+        {
+            if(r instanceof Baking)
+            {
+                Baking b = (Baking) r;
+                if (b.containsEgg() == containsEgg)
+                out.add(b);
+            }
+        }
+        return out;
+    }
+    public ArrayList<Frying> getFryingByFry(boolean isDeep)
+    {
+        ArrayList<Frying> out = new ArrayList<>();
+        for(Recipe r : recipes)
+        {
+            if(r instanceof Frying)
+            {
+                Frying f = (Frying) r;
+                if(f.isDeep() == isDeep)
+                out.add(f);
+            }
+        }
+                return out;
     }
 }
