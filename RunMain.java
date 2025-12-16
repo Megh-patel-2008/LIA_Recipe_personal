23b4e3d41be892316e0e79cf976fcf3e29952db7
Megh-patel-2008 <M3ghpatel2008@gmail.com>
2025-12-16 17:20:52 -0500

    Final push

--- RunMain.java
+++ RunMain.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
  */
 public class RunMain
 {
-    public static void RunMain(String[] args)
+    public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
         RecipeBook book = new RecipeBook();
